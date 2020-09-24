00fd9b5c5408fb472062bc6ac0b4aa284bd70213
Gabriel C <gabriel@winterwind.com>
2020-09-25 04:44:35 +0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-uploads-gab"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "ttps://fe1dosmdta.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_i2CAFSXOj",
+        APP_CLIENT_ID: "5i049u1nt2a176poed31pgvr5k",
+        IDENTITY_POOL_ID: "us-east-1:53316efa-b053-4a5d-bf14-590575c444d4"
+    }
+}
