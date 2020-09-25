681d13fdb20023aef473f86a3d32219b4b73d43e
Gabriel C <gabriel@winterwind.com>
2020-09-26 00:00:53 +0800

    + new note ui

--- src/config.js
+++ src/config.js
@@ -12,5 +12,6 @@ export default {
         USER_POOL_ID: "us-east-1_i2CAFSXOj",
         APP_CLIENT_ID: "5i049u1nt2a176poed31pgvr5k",
         IDENTITY_POOL_ID: "us-east-1:53316efa-b053-4a5d-bf14-590575c444d4"
-    }
+    },
+    MAX_ATTACHMENT_SIZE: 5000000
 }
